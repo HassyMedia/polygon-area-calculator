58e29c01968a2cafd7d133398bdaddd5e5a23a46
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-02 23:49:35 -0500

    Initialized Rectangle class with constructor.

---
+++ poly_calculator.py
@@ -0,0 +1,5 @@
+class Rectangle:
+    # Constructor: Initializes a new instance of the Rectangle class with width and height.
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
