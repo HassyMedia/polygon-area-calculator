1cd1f1addd588286b75fe2b4251f86fc7c1c2036
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-02 23:52:50 -0500

    Added width and height setter methods to Rectangle class.

--- poly_calculator.py
+++ poly_calculator.py
@@ -3,3 +3,11 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+    # Method to set the width of the rectangle.
+    def set_width(self, width):
+        self.width = width
+
+    # Method to set the height of the rectangle.
+    def set_height(self, height):
+        self.height = height
